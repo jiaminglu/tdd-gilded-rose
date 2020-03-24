9937c3d914b389d907f3557bc59ae9d30fb80d88
Jiaming Lu <>
2020-03-24 15:57:16 +0800

    write test

---
+++ src/main/java/cn/xpbootcamp/gilded_rose/Item.java
@@ -0,0 +1,5 @@
+package cn.xpbootcamp.gilded_rose;
+
+public interface Item {
+    int manufactureDate();
+}
---
+++ src/test/java/cn/xpbootcamp/gilded_rose/ShopTest.java
@@ -0,0 +1,40 @@
+package cn.xpbootcamp.gilded_rose;
+
+import org.junit.jupiter.api.Test;
+
+public class ShopTest {
+    @Test
+    public void allItemsHaveNonNegativeQuality() {
+
+    }
+
+    @Test
+    public void itemsPassesSellInDateQualityDecreaseAtDoubleRate() {
+
+    }
+
+    @Test
+    public void agedBrieQualityIncreaseWhenDateIncrease() {
+
+    }
+
+    @Test
+    public void sulfurasHaveUnchangingQuality() {
+
+    }
+
+    @Test
+    public void backstagePassQualityIncreaseBeforeStageDate() {
+
+    }
+
+    @Test
+    public void backstagePassQualityIncreaseFasterWhenApproachingStageDate() {
+
+    }
+
+    @Test
+    public void backstagePassQualityBecomeZeroAfterStageDate() {
+
+    }
+}
